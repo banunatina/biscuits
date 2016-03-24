5e8faf41810a15db3a048e4db99dafa614a654b5
Banun Atina Idris <banunatina@gmail.com>
2016-03-24 15:30:48 -0700

    Add bstBalance (no tests).

---
+++ prompts/bstBalance/prompt.js
@@ -0,0 +1,13 @@
+/*
+ *
+ * Implement a function that checks if a binary search tree is balanced.
+ * In this case, a binary search tree is balanced if the heights of the two subtrees
+ * of any node never differ by more than 1.
+ *
+ */
+
+var isBSTBalanced = function(tree) {
+
+};
+
+module.exports = isBSTBalanced;
