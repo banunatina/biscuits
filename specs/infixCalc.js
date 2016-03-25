391bb6f518b029a0353a00efacbd16c3190d4b72
Yilen <yilen.pan@gmail.com>
2016-03-24 17:53:48 -0700

    added spec

--- specs/infixCalc.js
+++ specs/infixCalc.js
@@ -2,7 +2,16 @@ var expect = require('chai').expect;
 var infixCalc = require('../prompts/infixCalc/solution');
 describe('infixCalc', function (done) {
   it('should calculate an infix string', function (done) {
-    var infix = "1 + 2 * (20 / 5)";
-    expect(infixCalc(infix)).to.equal(eval(infix));
+    var infixes = [
+      "1 + 2 * (20 / 5)",
+      "( ( 4 * 3 + 1 * 3 ) / 5 + 1 ) + 8",
+      "1 + 3 + ( ( 4 / 2 ) * ( 8 * 4 ) )",
+      "( 8 * 3 ) + 2",
+      "1 + 2 * ( 20 / 5 )"
+    ];
+    var truth = infixes.every(function (infix) {
+      return infixCalc(infix) === eval(infix);
+    });
+    expect(truth).to.equal(true);
   });
 });
