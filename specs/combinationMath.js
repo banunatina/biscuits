6c7aefe26462b574889a24c634b61d42719411a1
Yilen <yilen.pan@gmail.com>
2016-04-02 09:46:42 -0700

    oopsie

--- specs/combinationMath.js
+++ specs/combinationMath.js
@@ -5,7 +5,7 @@ describe('should find combinations that equal a target', function (done) {
   it('should find all of the combinations', function (done) {
     var result = combinationMath('123123', 0); // going to get length
     var expected = 63;
-    expect(result.length).to.equal(result.length);
+    expect(result.length).to.equal(expected);
     done();
   });
 });
