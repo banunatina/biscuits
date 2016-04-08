65086aa19d6fc95de22be96764c7499f75ee09aa
Yilen <yilen.pan@gmail.com>
2016-04-08 16:08:00 -0700

    deleteANodeBST

---
+++ specs/deleteANodeBST.js
@@ -0,0 +1,19 @@
+var BST = require('../ds').BinaryTree;
+var deleteNode = require('../prompts/deleteANodeBST');
+
+
+describe('Delete a node', function (done) {
+  var bst;
+  beforeEach(function (done) {
+    bst = [12,8,16,10,6,14,18,9].reduce(function (tree, value) {
+      tree.insert(value);
+      return tree;
+    }, new BST());
+  });
+
+  it('should delete the root node', function (done) {
+    deleteNode(bst.root);
+    var expected = bst.root.value === 14 || bst.root.value === 10;
+    expect(expected).to.equal(true);
+  });
+});
