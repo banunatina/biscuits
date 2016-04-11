42c84407e6677892466c4899a73cbcfd4d25a62b
Yilen <yilen.pan@gmail.com>
2016-04-11 16:31:14 -0700

    findEquilibrium

--- prompts/findEquilibrium/prompt.js
+++ prompts/findEquilibrium/prompt.js
@@ -8,8 +8,11 @@
 //
 /////////////////////////////////////////////////////////////
 
-var findEquilibriumIndex = function () {
+var findEquilibriumIndex = function (array) {
   // TODO: Implement me
 };
+
+// [1,2,3,6] => 2
+// [1,2,3,4,5,6,7,8,9,11] => 6
 
 module.exports = findEquilibriumIndex;
